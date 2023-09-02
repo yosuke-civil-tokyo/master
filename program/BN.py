6d8d48cf2f1b27ac9cf9ecd7a38fac2f4430d27f
yosuke-civil-tokyo <moon.and.sun3663@gmail.com>
2023-09-03 01:00:30 +0900

    cpt estimation from data

--- program/BN.py
+++ program/BN.py
@@ -6,6 +6,7 @@ class Variable:
         self.states = states  # The number of possible states this variable can take
         self.parents = []  # List to store parent variables
         self.cpt = None  # Conditional Probability Table as a NumPy array
+        self.data = None  # Field to store observed data
 
     def set_parents(self, parents):
         self.parents = parents
@@ -13,38 +14,57 @@ class Variable:
     def set_cpt(self, cpt_array):
         self.cpt = np.array(cpt_array)
 
+    def set_data(self, data_array):
+        self.data = np.array(data_array)
+
     def probability(self, parent_states):
-        # Create an index tuple to access the correct element in the CPT NumPy array
+        # Create an index tuple to access the correct slice in the CPT NumPy array
         index = tuple(parent_states.tolist())
-        
-        # Fetch the probability from the CPT using the index
+        # Fetch the probabilities from the CPT using the index
         return self.cpt[index]
+    
+    def estimate_cpt(self):
+        if self.data is None:
+            raise ValueError("Data not set for this variable.")
+        
+        num_states = [parent.states for parent in self.parents] + [self.states]
+        
+        # Initialize CPT with zeros
+        self.cpt = np.zeros(num_states)
+        
+        # Compute the dimensions of the parent data
+        parent_dims = [parent.data for parent in self.parents]
+        
+        # Calculate joint counts
+        for i, val in enumerate(self.data):
+            index = tuple(parent[i] for parent in parent_dims) + (val,)
+            self.cpt[index] += 1
+        
+        # Normalize to get probabilities
+        self.cpt /= self.cpt.sum(axis=-1, keepdims=True)
 
 
-if __name__=="__main__":
-    # Create variables A, B, and C with 2 states each
+if __name__ == "__main__":
+    # Simulated data for demonstration
+    np.random.seed(0)
+    data_A = np.random.choice([0, 1], size=1000)
+    data_B = np.random.choice([0, 1], size=1000)
+    data_C = np.random.choice([0, 1], size=1000)
+
+    # Create Variables
     A = Variable("A", 2)
     B = Variable("B", 2)
     C = Variable("C", 2)
 
+    # Set data
+    A.set_data(data_A)
+    B.set_data(data_B)
+    C.set_data(data_C)
+
     # Set parents for C
     C.set_parents([A, B])
 
-    # Create a CPT for C using a NumPy array
-    # This is a 2x2x2 array representing P(C | A, B)
-    # The last dimension represents the state of C, while the first and second dimensions represent the states of A and B
-    cpt_C = np.array([[[0.1, 0.9], [0.4, 0.6]], [[0.7, 0.3], [0.2, 0.8]]])
+    # Estimate CPT for C
+    C.estimate_cpt()
 
-    # Set the CPT for C
-    C.set_cpt(cpt_C)
-
-    # Calculate probabilities
-    parent_states_A0_B0 = np.array([0, 0])
-    prob_C_given_A0_B0 = C.probability(parent_states_A0_B0)
-
-    parent_states_A1_B1 = np.array([1, 1])
-    prob_C_given_A1_B1 = C.probability(parent_states_A1_B1)
-
-    print(f"P(C| A=0, B=0) = {prob_C_given_A0_B0}")
-    print(f"P(C| A=1, B=1) = {prob_C_given_A1_B1}")
-
+    print("Estimated CPT for C:", C.cpt)
