4721a7d26f6fee37541dc6bf1cca424c116d1dfc
yosuke-civil-tokyo <moon.and.sun3663@gmail.com>
2023-11-06 12:42:20 +0900

    manage and rerun test

--- program/TestCase.py
+++ program/TestCase.py
@@ -1,91 +1,70 @@
 # list the experiment case for OOBN
+import argparse
 # packages
+from cfg.GlobalTest import Configs
 from DataLoader import make_dataloader
 from OOBN import ObjectNode
 from BN import Variable
 from DataClalss import pt_data_types, walk_data_types, car_data_types
 
 # example test case
-def exTest():
-    # Name of the case
-    case_name = "example2"
-    # Define the case
-    data_files = [
-        'data/activityData/MS2611_utf8.csv',
-        'data/losData/05_代表徒歩_現況.csv',
-        # 'data/losData/03_代表自動車_現況_utf8.csv',
-        'data/losData/01_代表鉄道_現況_utf8.csv',
-    ]
-    convert_dict = {
-        "自動車運転免許保有の状況": {0: 0, 1: 1, 2: 1},
-        "代表交通手段：分類０": {1: 0, 5: 1},
-        "移動の目的": {
-            1: 0, 2: 0, 3: 1, 4: 2, 5: 2, 6: 2, 7: 2, 8: 2, 9: 2, 10: 2, 19: 3
-        },
-        "目的種類：分類１": {
-            2: 0, 3: 0, 4: 0, 5: 1, 6: 1, 7: 0, 8: 0, 9: 0, 10: 1, 11: 1, 13: 2, 14: 1, 15: 0, 16: 0, 17: 1, 18: 1
-        },
-    }
-
-    convert_dict_continuous = {
-        "徒歩所要時間（分）": 3,
-        #"時間（分）": 3,
-        "鉄道乗車時間〔分〕": 3,
-        "トリップ数": 3,
-        "年齢": 3,
-        "同行人数：人数": 3,
-    }
-
-    change_name_dict = {
-        # "自動車運転免許保有の状況": "License",
-        "トリップ数": "TripNumber",
-        "代表交通手段：分類０": "Mode",
-        "移動の目的": "Purpose",
-        "目的種類：分類１": "PurposeType",
-        "年齢": "Age",
-        "同行人数：人数": "Companion",
-        "徒歩所要時間（分）": "WalkTime",
-        "鉄道乗車時間〔分〕": "TrainTime",
-        # "時間（分）": "CarTime",
-    }
-
-    # Define the columns for ObjectNode 1 and 2
-    col_obj1 = ["Age", "Companion", "Purpose", "PurposeType"]
-    col_obj2 = ["TripNumber", "WalkTime", "TrainTime", "Mode"]
+def exTest(config):
+    case_name = config["case_name"]
+    data_files = config["data_files"]
+    convert_dict = config["convert_dict"]
+    convert_dict_continuous = config["convert_dict_continuous"]
+    change_name_dict = config["change_name_dict"]
+    object_configs = config["objects"]
 
     # Load data using the new make_dataloader function
     print("Loading data...")
     dl = make_dataloader(data_files, convert_dict, convert_dict_continuous, change_name_dict, case_name)
     print("data num : ", len(dl.pt_data))
 
-    # Create ObjectNode instances
-    print("Making ObjectNodes...")
-    obj1 = ObjectNode("obj1", {})
-    obj1.set_data_from_dataloader(dl, col_obj1)
-    
-    obj2 = ObjectNode("Mode", {})
-    obj2.set_data_from_dataloader(dl, col_obj2)
+    # list of object
+    objects = {}
 
-    # Set input/output data for obj2
-    obj2.set_data(obj2.variables["TripNumber"].get_data(), 'input')
-    obj2.set_data(obj2.variables["Mode"].get_data(), 'output')
+    # Iterate through the object configurations and initialize ObjectNode instances
+    for obj_conf in object_configs:
+        print(f"Making {obj_conf['name']}...")
+        obj_columns = [change_name_dict.get(var, var) for var in obj_conf["variables"]]  # Handle renamed columns
+        objects[obj_conf['name']] = ObjectNode(obj_conf['name'], {})
+        objects[obj_conf['name']].set_data_from_dataloader(dl, obj_columns)
 
-    # Set obj2 as a variable in obj1
-    obj1.add_variable(obj2)
+        # If the object has defined input/output variables, set them
+        if "input" in obj_conf and "output" in obj_conf:
+            input_var = change_name_dict.get(obj_conf["input"], obj_conf["input"])
+            output_var = change_name_dict.get(obj_conf["output"], obj_conf["output"])
+            objects[obj_conf['name']].set_data(objects[obj_conf['name']].variables[input_var].get_data(), 'input')
+            objects[obj_conf['name']].set_data(objects[obj_conf['name']].variables[output_var].get_data(), 'output')
 
-    print("Estimation starts...")
-    fixed_positions_obj1 = {"Age": 0, "Purpose": 1}
-    obj1.structure_optimization(fixed_positions_obj1)
+    # Now, iterate again to set up object relationships after all have been initialized
+    for obj_conf in object_configs:
+        if "objs" in obj_conf:
+            for child_name in obj_conf["objs"]:
+                objects[obj_conf['name']].add_variable(objects[child_name])
 
-    fixed_positions_obj2 = {"TripNumber": 0, "Mode": 4}
-    obj2.structure_optimization(fixed_positions_obj2)
+    # Perform structure optimization based on fixed positions
+    for obj_conf in object_configs:
+        fixed_positions = {change_name_dict.get(k, k): v for k, v in obj_conf["fix"].items()}
+        print(f"Structure optimization for {obj_conf['name']}...")
+        objects[obj_conf['name']].structure_optimization(fixed_positions)
 
-    # Display the optimized structure
-    print("Visualizing structure for obj1...")
-    obj1.visualize_structure()
-    
-    print("Visualizing structure for obj2...")
-    obj2.visualize_structure()
+    # Visualize the structures
+    for obj_conf in object_configs:
+        print(f"Visualizing structure for {obj_conf['name']}...")
+        objects[obj_conf['name']].visualize_structure()
+
 
 if __name__ == "__main__":
-    exTest()
+    parser = argparse.ArgumentParser(description="Run an experiment")
+
+    # add the arguments
+    parser.add_argument("CaseName",
+                        metavar="case_name",
+                        type=str,
+                        help="the name of the case to run")
+    args = parser.parse_args()
+    config = Configs[args.CaseName]
+
+    exTest(config)
