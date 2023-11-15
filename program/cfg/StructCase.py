72169b29690a21966c506871bcdb89589215ec10
yosuke-civil-tokyo <moon.and.sun3663@gmail.com>
2023-11-15 18:04:34 +0900

    test case mod

--- program/cfg/StructCase.py
+++ program/cfg/StructCase.py
@@ -12,7 +12,7 @@ Configs = {
         ],
         "convert_dict": {
             "自動車運転免許保有の状況": {0: 1, 1: 2, 2: 2},
-            "代表交通手段：分類０": {1: 1, 5: 2},
+            "代表交通手段：分類０": {1: 1, 5: 2, 10: 3},
             "移動の目的": {
                 1: 1, 2: 1, 3: 2, 4: 3, 5: 3, 6: 3, 7: 3, 8: 3, 9: 3, 10: 3, 19: 4
             },
@@ -44,6 +44,39 @@ Configs = {
             {"name": "whole",
               "variables": ["Age", "Companion", "Purpose", "PurposeType", "TripNumber", "WalkTime", "TrainTime", "CarTime", "License", "Mode"],
               "fix": {},
+            }
+        ],
+        "numrows": 1000,
+        "evaluate_target": "Mode",
+    },
+    "fast1": {
+        "case_name": "fast1",
+        "data_files": [
+            'data/activityData/MS2611_utf8.csv',
+            'data/losData/05_代表徒歩_現況.csv',
+            'data/losData/03_代表自動車_現況_utf8.csv',
+            'data/losData/01_代表鉄道_現況_utf8.csv'
+        ],
+        "convert_dict": {
+            "自動車運転免許保有の状況": {0: 1, 1: 2, 2: 2},
+            "代表交通手段：分類０": {1: 1, 5: 2, 10: 3},
+        },
+        "convert_dict_continuous": {
+            "鉄道乗車時間〔分〕": 5,
+            "トリップ数": 10,
+            "同行人数：人数": 3,
+        },
+        "change_name_dict": {
+            "自動車運転免許保有の状況": "License",
+            "トリップ数": "TripNumber",
+            "代表交通手段：分類０": "Mode",
+            "同行人数：人数": "Companion",
+            "鉄道乗車時間〔分〕": "TrainTime",
+        },
+        "objects": [
+            {"name": "whole",
+              "variables": ["Companion", "TripNumber", "TrainTime", "License", "Mode"],
+              "fix": {"Mode": 5},
             }
         ],
         "numrows": 10000,
